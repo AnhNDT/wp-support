fd1c9289c378194ebde6a3ddf18faa70e899ffb3
AnhNDT@vn.innovation.co.jp <AnhNDT@vn.innovation.co.jp>
2015-01-21 15:03:31 +0700

    tag

--- source/wp-content/themes/support/functions-tag.php
+++ source/wp-content/themes/support/functions-tag.php
@@ -2,53 +2,87 @@
 
 /**
  * TAGS
+ * ref. 
+ * http://adambrown.info/p/wp_hooks/hook/edit_%7B$taxonomy%7D
+ * http://wpseek.com/hook/delete_taxonomy/
  * https://core.trac.wordpress.org/ticket/25532
+ * http://wordpress.stackexchange.com/questions/28467/custom-columns-on-edit-tags-php-main-page
+ * http://sabramedia.com/blog/how-to-add-custom-fields-to-custom-taxonomies
+ */
+
+/**
  *
  */
-add_action('post_tag_edit_form', 'foo_render_extra_fields');
-function foo_render_extra_fields() {
-	$term_id = $_GET['tag_ID'];
-	$term = get_term_by('id', $term_id, 'taxonomy');
-	$meta = get_option("taxonomy_{$term_id}");
-	//Insert HTML and form elements here
-	//var_dump($term_id);
+function action_created_post_tag_save_tag_css($term_id, $tt_id) {
+	$tag_ID = $term_id;
+	//$taxonomy = $_REQUEST['taxonomy']; // 'post_tag'
+	$custom_tag_css = $_REQUEST['custom_tag_css'];
+	update_option("post_tag_{$tag_ID}", $custom_tag_css);
 }
+add_action('created_post_tag', 'action_created_post_tag_save_tag_css', 10, 2);
 
-add_action('edited_taxonomy', 'bar_save_extra_fields', 10, 2);
-function bar_save_extra_fields($term_id){
-	$form_field_1 = $_REQUEST['field-name-1'];
-	$form_field_2 = $_REQUEST['field-name-2'];
-	$meta['key_value_1'] = $form_field_1;
-	$meta['key_value_2'] = $form_field_2;
-	//update_option("taxonomy_{$term_id}", $meta);
-	var_dump($_REQUEST);
+function action_edited_post_tag_save_tag_css() {
+	$tag_ID = $_REQUEST['tag_ID'];
+	//$taxonomy = $_REQUEST['taxonomy']; // 'post_tag'
+	$custom_tag_css = $_REQUEST['custom_tag_css'];
+	update_option("post_tag_{$tag_ID}", $custom_tag_css);
 }
+add_action('edited_post_tag', 'action_edited_post_tag_save_tag_css');
 
-// edit_tag_form, edit_tag_form_fields, `$taxonomy . '_edit_form_fields'
-add_action('edit_tag_form_fields', 'action_edit_tag_form_fields');
+function action_delete_post_tag_save_tag_css($term_id, $tt_id, $deleted_term) {
+	$tag_ID = $term_id;
+	delete_option("post_tag_{$tag_ID}");
+}
+add_action('delete_post_tag', 'action_created_post_tag_save_tag_css', 10, 3);
+
+/**
+ *
+ */
 function action_edit_tag_form_fields() {
-	$term_id = $_GET['tag_ID'];
-	$term = get_term_by('id', $term_id, 'taxonomy');
-	$meta = get_option("taxonomy_{$term_id}");
-	$meta_tag_color = <<<EOT
+	$tag_ID = $_GET['tag_ID'];
+	//$term = get_term_by('id', $term_id, 'taxonomy');
+	$custom_tag_css = get_option("post_tag_{$tag_ID}");
+	$tag_css_html = <<<EOT
 <tr class="form-field term-slug-wrap">
-	<th scope="row"><label for="color">Color</label></th>
-				<td><input name="color" id="tag-color" value="" size="40" type="text">
-	<p class="description">The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.</p></td>
+	<th scope="row"><label for="custom_tag_css">CSS</label></th>
+				<td><input name="custom_tag_css" id="custom_tag_css" value="{$custom_tag_css}" size="40" type="text">
+	<p class="description">The CSS is text format for viewing more attractive.</p></td>
 </tr>
 EOT;
-	echo $meta_tag_color;
+	echo $tag_css_html;
 }
+add_action('edit_tag_form_fields', 'action_edit_tag_form_fields'); // edit_tag_form, edit_tag_form_fields, `$taxonomy . '_edit_form_fields'
 
-// add_tag_form, add_tag_form_fields, `$taxonomy . '_add_form_fields'
-add_action('add_tag_form_fields', 'action_add_tag_form_fields');
+/**
+ *
+ */
 function action_add_tag_form_fields() {
-	$meta_tag_color = <<<EOT
+	$tag_css_html = <<<EOT
 <div class="form-field term-slug-wrap">
-	<label for="tag-color">Color</label>
-	<input name="color" id="tag-color" value="" size="40" type="text">
-	<p>The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.</p>
+	<label for="custom_tag_css">CSS</label>
+	<input name="custom_tag_css" id="custom_tag_css" value="" size="40" type="text">
+	<p>The CSS is text format for viewing more attractive.</p>
 </div>
 EOT;
-	echo $meta_tag_color;
+	echo $tag_css_html;
 }
+add_action('add_tag_form_fields', 'action_add_tag_form_fields'); // add_tag_form, add_tag_form_fields, `$taxonomy . '_add_form_fields'
+
+/**
+ *
+ */
+function filter_post_tag_columns($columns){
+	$columns['CSS'] = 'CSS';
+	return $columns;
+}
+add_filter('manage_edit-post_tag_columns', 'filter_post_tag_columns');
+
+function filter_post_tag_columns_content($value, $column, $tag_ID){
+	$custom_tag_css = get_option("post_tag_{$tag_ID}");
+	if (isset($custom_tag_css)) {
+		$value .= $custom_tag_css;
+	}
+	return $value;
+}
+add_filter('manage_post_tag_custom_column', 'filter_post_tag_columns_content', 10, 3);
+
--- source/wp-content/themes/support/functions.php
+++ source/wp-content/themes/support/functions.php
@@ -12,86 +12,9 @@ function no_sidebar() {
 }
 
 /**
- * disable autosave of POST
- */
-function disable_autosave() {
-	wp_deregister_script('autosave');
-}
-add_action('wp_print_scripts', 'disable_autosave');
-
-/**
- * customize post type
- */
-function add_custom_post_type() {
-	_register_custom_post_type('NOTICE', 'notice');
-	_register_custom_post_type('NEWS', 'news');
-}
-function add_custom_post_type_faq() {
-	_register_custom_post_type_faq('FAQ', 'faq');
-}
-function _register_custom_post_type($label, $slag) {
-	register_post_type($slag,
-		array(
-			'label' => $label,
-			'labels' => array(
-				'name' => $label,
-				'singular_name' => $label,
-				//'add_new' => $label . 'を追加',
-				//'new_item' => '新しい' . $label,
-				//'view_news' => $label . 'を表示',
-				//'not_found' => $label . 'は見つかりませんでした',
-				//'not_found_in_trash' => 'ゴミ箱に' . $label . 'はありません。',
-				//'search_items' => $label . 'を検索',
-			),
-			'public' => true,
-			'show_ui' => true,
-			'has_archive' => true,
-			'capability_type' => 'post',
-			'hierarchical' => true,
-			'menu_position' => 5,
-			'supports' => array('title', 'editor', 'author', 'thumbnail', 'comments', 'custom-fields',),
-			'yarpp_support' => true, // YARPPプラグイン用
-			'query_var' => true,
-			'rewrite' => array('slug' => $slag, 'with_front' => false),
-		)
-	);
-	flush_rewrite_rules();
-}
-function _register_custom_post_type_faq($label, $slag) {
-	register_post_type($slag,
-		array(
-			'label' => $label,
-			'labels' => array(
-				'name' => $label,
-				'singular_name' => $label,
-				//'add_new' => $label . 'を追加',
-				//'new_item' => '新しい' . $label,
-				//'view_news' => $label . 'を表示',
-				//'not_found' => $label . 'は見つかりませんでした',
-				//'not_found_in_trash' => 'ゴミ箱に' . $label . 'はありません。',
-				//'search_items' => $label . 'を検索',
-			),
-			'public' => true,
-			'show_ui' => true,
-			'has_archive' => true,
-			'capability_type' => 'post',
-			'hierarchical' => true,
-			'menu_position' => 5,
-			'supports' => array('title', 'editor', 'author', 'thumbnail', 'comments', 'custom-fields',),
-			'yarpp_support' => true, // YARPPプラグイン用
-			'query_var' => true,
-			'rewrite' => array('slug' => $slag, 'with_front' => false),
-			'taxonomies' => array('post_tag', 'category'),
-		)
-	);
-	flush_rewrite_rules();
-}
-add_action('init', 'add_custom_post_type_faq');
-add_action('init', 'add_custom_post_type');
-
-/**
  *
  */
 if (is_admin()) {
+	include_once 'functions-post-type.php';
 	include_once 'functions-tag.php';
 }
